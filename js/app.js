8db87cd066e260929aac37a51f33f04676a60c6b
osaramee <osaramee@gmail.com>
2025-01-20 19:25:00 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// カーソル用のdivタグを取得してcursorに格納
+var cursor = document.getElementById('cursor');
+
+// カーソル用のdivタグをマウスに追従させる
+document.addEventListener('mousemove', function (e) {
+    cursor.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+});
+
+// リンクにホバーした時にクラス追加、離れたらクラス削除
+var link = document.querySelectorAll('a');
+for (var i = 0; i < link.length; i++) {
+    link[i].addEventListener('mouseover', function (e) {
+        cursor.classList.add('cursor--hover');
+    });
+    link[i].addEventListener('mouseout', function (e) {
+        cursor.classList.remove('cursor--hover');
+    });
+}
